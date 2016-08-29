414d6ea22b9d1113a73528fed38d191563a4f54f
tfennelly <tom.fennelly@gmail.com>
2016-08-29 14:59:21 +0100

    Initial gh-pages and publish script

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pushd pages
+
+git init
+git remote add webpages git@github.com:smooks/web.git
+git fetch --depth=1 webpages gh-pages
+
+git add --all
+git commit -m "webpages"
+git merge --no-edit -s ours remotes/webpages/gh-pages
+
+git push webpages master:gh-pages
+
+rm -rf .git
+
+popd
