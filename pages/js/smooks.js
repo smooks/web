7fd144d3a7b7cc324aab9a5483776af9f3defd99
tfennelly <tom.fennelly@gmail.com>
2016-08-31 12:44:47 +0100

    Embedding user mailing list in community page

--- pages/js/smooks.js
+++ pages/js/smooks.js
@@ -1,13 +1,15 @@
 (function(){
     
-    // Page sizing when there's an iframe.
-    function setPageSize() {
-        $('body').height($(window).height() - 150);
-    }
-    setPageSize();
-    $(window).resize(function() {
+    // Page sizing when there's a "full page" iframe on it.
+    if ($('.full-page-iframe').length > 0) {
+        function setPageSize() {
+            $('body').height($(window).height() - 150);
+        }
         setPageSize();
-    });
+        $(window).resize(function() {
+            setPageSize();
+        });
+    }
     
     // Highlight the right nav
     var nav = $("span[data-page-nav]");
